ee33a3ff433dbc0ace01e4535748c0ded342247b
Shervin Lepinat <shervin_lepinat@bluewin.ch>
2023-12-18 15:40:24 +0100

    navigation first

--- src/app/components/shared/nav/nav.component.ts
+++ src/app/components/shared/nav/nav.component.ts
@@ -8,4 +8,13 @@ import {Constants} from "../../../../helper/classes/Constants";
 })
 export class NavComponent {
   public elements: string[] = Constants.Sites.SITES;
+  private mode: 'light' | 'dark' = 'dark';
+
+  public getCurrentModeSrc(): string {
+    const base: string = 'assets/icons/mode/'
+    if (this.mode === 'dark') {
+      return base + 'moon.svg';
+    }
+    return base + 'brightness.svg';
+  }
 }
