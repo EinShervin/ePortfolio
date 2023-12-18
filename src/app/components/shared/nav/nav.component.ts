99cac6b47b31cb2a816671cf199d0b858ab0c337
Shervin Lepinat <shervin_lepinat@bluewin.ch>
2023-12-18 16:12:52 +0100

    hover statements

--- src/app/components/shared/nav/nav.component.ts
+++ src/app/components/shared/nav/nav.component.ts
@@ -1,20 +1,32 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {Constants} from "../../../../helper/classes/Constants";
 
 @Component({
-  selector: 'app-nav',
-  templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+    selector: 'app-nav',
+    templateUrl: './nav.component.html',
+    styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
-  public elements: string[] = Constants.Sites.SITES;
-  private mode: 'light' | 'dark' = 'dark';
+    public elements: string[] = Constants.Sites.SITES;
+    private mode: 'light' | 'dark' = 'dark';
+    public activeElement: string = Constants.Sites.HOME;
 
-  public getCurrentModeSrc(): string {
-    const base: string = 'assets/icons/mode/'
-    if (this.mode === 'dark') {
-      return base + 'moon.svg';
+    public getCurrentModeSrc(): string {
+        const base: string = 'assets/icons/mode/'
+        if (this.mode === 'dark') {
+            return base + 'moon.svg';
+        }
+        return base + 'brightness.svg';
     }
-    return base + 'brightness.svg';
-  }
+
+    public getCurrentMode(): string {
+        if (this.mode === 'light') {
+            return 'Lightmode';
+        }
+        return "Darkmode";
+    }
+
+    public switchMode(): void {
+        this.mode = this.mode === 'light' ? 'dark' : 'light';
+    }
 }
