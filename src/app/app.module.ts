b1623690676e37cbff718410d11aca3e8a8f9f23
Shervin Lepinat <shervin_lepinat@bluewin.ch>
2023-12-20 05:37:21 +0100

    button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import {NavComponent} from "./components/shared/nav/nav.component";
 import { ContentSliderComponent } from './components/content-slider/content-slider.component';
 import { ContentComponent } from './components/content/content.component';
+import { ButtonComponent } from './components/shared/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     ContentSliderComponent,
-    ContentComponent
+    ContentComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule
